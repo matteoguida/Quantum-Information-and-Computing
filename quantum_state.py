22b955d4ed6a47d78abe401a92697b3100c8b6a0
albchim <chimenti.alberto@gmail.com>
2020-10-23 15:26:59 +0200

    unexpected behaviour with short ime windows to be verified

--- quantum_state.py
+++ quantum_state.py
@@ -12,6 +12,7 @@
 
 #%%
 import numpy as np
+from scipy.constants import hbar
 '''
     Global variable for easier use (BEWARE OF LOOP INDICES CHOICE)
 '''
@@ -28,51 +29,63 @@ def hamiltonian(field, single=True):
         pass
 
 
-def time_evolution(psi, dt, field, trotter=False):
+def time_evolution(psi, dt, field, euler=False):
     '''
-        Flexible implementation of time evolution of pure quantum states
-        for a variable number of qbits
-        --> To implement other options than Trotter-Suzuki decomposition for time
-            evolution unitary operator
+    Function to implement time evolution for a pure quantum state.
+    ! WARNING: as of 22.10.2020 exact method only implements time evolution 
+               according to the hamiltonian H = Sx + field*Sz for 1 QuBit.
+
+    Inputs:
+    # psi: np.array(dtype=complex), vector containing coefficients of the state to evolve
+    # dt: float, timestep
+    # field: float, magnetic field (parameter of the hamiltonian)
+    #euler: boolean, if "True" time evolution operator is approximated using euler method. 
+            If "False" exact method is used (spectral method).
+            Default value is "False".
+    Outputs:
+    # psi: np.array(dtype=complex), vector containing coefficients of the evolved state.
+           
     '''
-    if trotter:
+    if euler:
         identity = np.diag(np.ones(len(psi)))
         psi = np.dot((identity -i*hamiltonian(field)*dt), psi)
         return(psi)
     else:
+        #------------------------CLARA---------------------------------------------------------------------
         if len(psi) == 2: #this method only works for one qubit
             #implements evolution using spectral method for one qubit
-            #ep_state = np.array([(1 - np.sqrt(1 + field*field))/np.sqrt(2 * (1 + field*field - np.sqrt(1 + field*field))), 
-            #                    field / np.sqrt(2 * (1 + field*field - np.sqrt(1 + field*field)))], dtype=complex)
-            #em_state = np.array([(1 + np.sqrt(1 + field*field))/np.sqrt(2 * (1 + field*field + np.sqrt(1 + field*field))), 
-            #                    field / np.sqrt(2 * (1 + field*field + np.sqrt(1 + field*field)))], dtype=complex)
 
-            ep_state = np.array([(1 - np.sqrt(1 + field*field))/field, 1], dtype=complex)
-            em_state = np.array([(1 + np.sqrt(1 + field*field))/field, 1], dtype=complex)
-            #print(ep_state, "\n", em_state)
-
-            ep_state = ep_state/np.sqrt(np.vdot(ep_state, ep_state))
-            em_state = em_state/np.sqrt(np.vdot(em_state, em_state))
-            #print("----->",ep_state, "\n", em_state,"<-----\n\n")
-
-            ep_autoval = np.sqrt(1 + field**2)
-            em_autoval = -np.sqrt(1 + field**2)
-
+            #compute eigenstates of hamiltonian H = Sx + field*Sz and normalize them
+            ep_state = np.array([field + np.sqrt(1 + field*field), 1], dtype=complex)
+            ep_state=ep_state/np.sqrt(np.vdot(ep_state,ep_state))
+            em_state = np.array([field - np.sqrt(1 + field*field), 1], dtype=complex)
+            em_state=em_state/np.sqrt(np.vdot(em_state,em_state))
+            #compute eigenvalues
+            ep_autoval = np.sqrt(1 + field*field)
+            em_autoval = -np.sqrt(1 + field*field)
+            #compute projections of the state to evolve over the eigenstates
             cp = np.vdot(ep_state,psi)
             cm = np.vdot(em_state,psi)
-
-            psi= cp * np.exp(- i * ep_autoval * dt) * ep_state + cm * np.exp(- i * em_autoval * dt) * em_state
+            #compute evolved state
+            psi= cp * np.exp(- i * ep_autoval * dt / hbar) * ep_state + cm * np.exp(- i * em_autoval * dt / hbar) * em_state
             
             return psi
         else:
             pass
+        #---------------------------------------------------------------------------------------------------
 
 
-def fidelity(target, psi):
+def compute_fidelity(target, psi):
     F = np.abs(np.vdot(target, psi))
     return F
 
 
+
+
+
+
+
+###########################################################################################################
 if __name__ == "__main__":
 
     H = hamiltonian(-2)
@@ -82,18 +95,17 @@ if __name__ == "__main__":
     psi = np.array([+1/np.sqrt(2)+1/np.sqrt(2)*i,0. + 0.j])
 
     print(psi)
-    for h in [-4, -2, 2, 4]:
+    for h in [-4, -2, 0, 2, 4]:
         print("Field=", h)
         print("\n")
-        psi_trotter = time_evolution(psi, 0.05, h)
-        psi_spectral= time_evolution(psi, 0.05, h, trotter=False)
-
+        psi_trotter = time_evolution(psi, 0.05, h, euler=True)
         print("Trotter:", psi_trotter)
-        print("Fidelity_trotter", fidelity(psi_trotter, psi_trotter))
+        print("Fidelity_trotter", compute_fidelity(psi_trotter, psi_trotter))
         print("\n")
+        psi_spectral= time_evolution(psi, 0.05, h, euler=False)
         print("Spectral:", psi_spectral)
-        print("Fidelity_spectral", fidelity(psi_spectral, psi_spectral))
+        print("Fidelity_spectral", compute_fidelity(psi_spectral, psi_spectral))
         print("\n")
         print("\n")
-    print(fidelity(psi, psi))
+    print(compute_fidelity(psi, psi))
     print(1/np.sqrt(2))
