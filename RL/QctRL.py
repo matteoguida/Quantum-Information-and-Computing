bc6d8a70736f55103b15db3d72690f4bae8d2c58
ceminente <43959050+ceminente@users.noreply.github.com>
2020-11-03 14:12:20 +0100

    changes in ev function
    now hamiltonians are pre-computed and diagonalized

--- RL/QctRL.py
+++ RL/QctRL.py
@@ -43,7 +43,7 @@ class Agent:
             if np.shape(qtable)==[self.nstates, self.nactions]:
                 self.qtable = qtable
             else: 
-                print("WARNING ----> Qtable size doesn't match given arguments \n [nstates*nactions, nactions]=", [nstates*nactions, nactions], "\n Given:", np.shape(qtable))
+                print("WARNING ----> Qtable size doesn't match given arguments \n [nstates*nactions, nactions]=", [self.nstates*self.nactions, self.nactions], "\n Given:", np.shape(qtable))
 
     def _init_qtable(self):
         # initialize Q table [The indexing will be index(t)*len(h)+index(h)]
@@ -216,7 +216,7 @@ def get_time_grid(t_max, dt):
 
 
 ############################################################################
-
+#%%
 
 if __name__ == "__main__":
 
@@ -241,22 +241,26 @@ if __name__ == "__main__":
     qtarget = np.array([+1/2 + (np.sqrt(5))/2 ,1], dtype=complex)
     qstart=qstart/np.sqrt(np.vdot(qstart,qstart))
     qtarget=qtarget/np.sqrt(np.vdot(qtarget,qtarget))
-    dt = 0.05
+    t_max=3
+    n_steps=100
+    dt = t_max/n_steps
+    
 
-    model = quantum_model(qstart, qtarget, dt)
-    model._init_inthamiltonian(L=1)
+    L=1
+    g=1
     all_actions = [-4, 0, 4]
     starting_action = 0
-    t_max = 2.4
     episodes = 10001
-    replay_freq=50
-    replay_episodes=100
+    replay_freq=40
+    replay_episodes=40
 
-    time_map = get_time_grid(t_max, dt)
+    model = quantum_model(qstart, qtarget, dt, L, g, all_actions)
+
+   # time_map = get_time_grid(t_max, dt)
 
     agent_init ={
         # Agent's initialization
-        'nsteps' : len(time_map),                  # number of training episodes
+        'nsteps' : n_steps,                  # number of training episodes
         'nactions' : len(all_actions),  
         'discount' : 1,                            # exponential discount factor
         'max_reward' : 1,
@@ -270,10 +274,14 @@ if __name__ == "__main__":
     print("---> N_states=", agent_init['nsteps']*agent_init['nactions'])
     print("---> Actions=", all_actions)
 
-    a = 0.9
+    
     # alpha value and epsilon
-    alpha = np.ones(episodes) * a
-    epsilon = np.linspace(0.8, 0.01, episodes) #epsilon gets smaller i.e. action become more greedy as episodes go on
+    a=0.9
+    eta=0.6
+    alpha = np.linspace(a, eta, episodes)
+    epsilon = np.linspace(0.01, 7, episodes) #epsilon gets smaller i.e. action become more greedy as episodes go on
+    epsilon = np.exp(-epsilon)
+    print(epsilon)
 
     
     # initialize the agent
@@ -284,11 +292,13 @@ if __name__ == "__main__":
 
     #### VARIOUS VISUALIZATION TASKS ####
     print("Best protocol Reward: {}".format(learner.best_reward))
-#%%    
+
+
+#%%
     # plot reward results
     total_episodes=episodes+np.floor(episodes/replay_freq)*replay_episodes
 
-    fname = 'train_result'+'_'+str(a)+'.png'
+    fname = 'train_result'+'_'+str(a)+'_'+str(eta)+'_'+str(t_max)+'.png'
     fname = out_dir / fname
     plt.close('all')
     fig = plt.figure(figsize=(10,6))
@@ -298,10 +308,10 @@ if __name__ == "__main__":
     plt.savefig(fname)
     plt.show()
     plt.close(fig=fig)
-'''
+    '''
     fname = 'protocol'+str(t_max)+'-'+str(dt)+'.gif'
     fname = out_dir / fname
-    create_gif(best_path, qstart, qtarget, fname)'''
+    create_gif(learner.best_path, qstart, qtarget, fname)'''
 
     
 #%%           
--- RL/Qmodel.py
+++ RL/Qmodel.py
@@ -1,60 +1,55 @@
 #%%
 import numpy as np
 
-class hamiltonian_eig:
 
-    def __init__(self, L, g, field):
-        self.L=L
-        self.field=field
-        self.g=g
-        self.H=None
-        self.eigvect=None
-        self.eigvals=None
-        #this initializes H and its spectral quantities
-        self.compute_H_and_LA()
+def compute_H_and_LA(L, g, field):
+    from numpy import linalg as LA
+    #pauli matrices
+    sigma_x=1/2*np.array([[0,1],[1,0]], dtype=complex)
+    #sigma_y=np.array([[0,-1j],[1j,0]], dtype=complex) # INUTILE
+    sigma_z=1/2*np.array([[1,0],[0,-1]], dtype=complex)
+    sigma_z_interaction= np.kron(sigma_z,sigma_z)
 
+    #create the hamiltonian according to the number of qubits
+    if L == 1:
+        H = -field*sigma_x - g*sigma_z
+    else:
+        H1 = np.zeros((2**L,2**L))
+        H2 = np.zeros((2**L,2**L))
+        H3 = np.zeros((2**L,2**L))
 
-    def compute_H_and_LA(self):
-        from numpy import linalg as LA
-        #pauli matrices
-        sigma_x=1/2*np.array([[0,1],[1,0]], dtype=complex)
-        #sigma_y=np.array([[0,-1j],[1j,0]], dtype=complex) # INUTILE
-        sigma_z=1/2*np.array([[1,0],[0,-1]], dtype=complex)
-        sigma_z_interaction= np.kron(sigma_z,sigma_z)
+        for j in range(L-1):   
+            H1 = np.kron(np.identity(2**j),sigma_z_interaction)
+            H1 = np.kron(H1,np.identity(2**(L-j-2)))
 
-        #create the hamiltonian according to the number of qubits
-        if self.L == 1:
-            self.H = -self.field*sigma_x - self.g*sigma_z
-        else:
-            H1 = np.zeros((2**self.L,2**self.L))
-            H2 = np.zeros((2**self.L,2**self.L))
-            H3 = np.zeros((2**self.L,2**self.L))
+        for j in range(L):
+            H3 = np.kron(np.identity(2**j),sigma_x)
+            H3 = np.kron(H3,np.identity(2**(L-j-1)))
+            H2 = np.kron(np.identity(2**j),sigma_z)
+            H2 = np.kron(H2,np.identity(2**(L-j-1)))
 
-            for j in range(self.L-1):   
-                H1 = np.kron(np.identity(2**j),sigma_z_interaction)
-                H1 = np.kron(H1,np.identity(2**(self.L-j-2)))
+        H = -(H1 + g*H2 + field*H3)
+    #compute and assign spectral quantities
+    eigval, eigvect = LA.eig(H)
+    spectral_dict = {"eigval":eigval , "eigvect":eigvect}
+    return spectral_dict
 
-            for j in range(self.L):
-                H3 = np.kron(np.identity(2**j),sigma_x)
-                H3 = np.kron(H3,np.identity(2**(self.L-j-1)))
-                H2 = np.kron(np.identity(2**j),sigma_z)
-                H2 = np.kron(H2,np.identity(2**(self.L-j-1)))
-
-            self.H = -(H1 + self.g*H2 + self.field*H3)
-        #compute and assign spectral quantities
-        self.eigval, self.eigvect = LA.eig(self.H)
-    
 
 
 class quantum_model:
 
-    def __init__(self, qstart, qtarget, dt, history=True):
+    def __init__(self, qstart, qtarget, dt, L, g, h_list, history=True):
 
         self.qstart=qstart
         self.qtarget=qtarget
         self.dt=dt
         self.history=history
 
+        self.L = L
+        self.g = g
+        self.h_list=h_list
+
+        self._init_hamiltonian() #given self.h_list computes spectral quantities for each field value
         self.reset()
         
     
@@ -69,19 +64,23 @@ class quantum_model:
 
         self.fidelity=None
 
-        #create hamiltonian variable which will comprehen
-        self.hamiltonian=None
 
-    def _init_inthamiltonian(self, L, g=1):
-        self.L = L
-        self.g = g
+
+    def _init_hamiltonian(self):
+        '''This is a dictionary of dictionaries. 
+        H_spectral_dict[field] contains a dictionary whose labels are "eigval" and "eigvect" containing, infact, eigevalues and eigvectors of the
+        hamiltonian with that field value.'''
+
+        self.H_spectral_dict = {field : compute_H_and_LA(self.L, self.g, field) for field in self.h_list}
+
 
     def evolve(self, field):
-        self.hamiltonian = hamiltonian_eig(self.L, self.g, field)
-        c_i = np.array([np.vdot(self.hamiltonian.eigvect[:,i],self.qcurrent) for i in range(len(self.qcurrent))]) 
+        eigvect = self.H_spectral_dict[field]["eigvect"]
+        eigval = self.H_spectral_dict[field]["eigval"]                                                  
+        c_i = np.array([np.vdot(eigvect[:,i],self.qcurrent) for i in range(len(self.qcurrent))]) 
         temp_psi = []
         for i in range(len(self.qcurrent)):
-            temp_psi.append(c_i[i]*np.exp((-1j*self.hamiltonian.eigval[i]*self.dt))*self.hamiltonian.eigvect[:,i])
+            temp_psi.append(c_i[i]*np.exp((-1j*eigval[i]*self.dt))*eigvect[:,i])
         self.qcurrent = np.array(temp_psi).sum(axis=0)
         if self.history:
             self.qstates_history.append(self.qcurrent)
@@ -92,40 +91,38 @@ class quantum_model:
         return np.copy(self.fidelity)
 
 
-    '''IDEA DI UTILIZZO
-    Inizializzo Qmodel passandogli qtarget,qstate e dt (! ad inizio training)
+    def evolve_from_protocol(self, protocol, make_gif=None):
+        
+        history_bool = self.history
+        if not self.history:
+            self.history=True
 
-    quando devo evolvere chiamo Qmodel.evolve(L, field, g): la funzione crea una classe hamiltonian che a sua volte sia rimpie calcolando 
-    H e le sue quantità spettrali. Lo stato current (Qmodel.qcurrent) viene aggiornato.
+        for h in protocol:
+            self.evolve(h)
 
-    quando devo calcolare la fidelity chiamo Qmodel.compute_fidelity() (! prima devo evolvere un'ultima volta) che restituisce appunto la fidelity
+        self.history=history_bool
 
-    La classe hamiltonian e la classe Qmodel sono seprate perché Qmodel tiene traccia degli stati e della loro evoluzione mentre la classe 
-    hamiltonian si occupa di gestire i cambiamenti dell'hamiltoniana CHE VA CAMBIATA OGNI VOLTA CON H!
+        return np.copy(self.qstates_history)
 
-    DOMANDE:
-    -ha senso tenersi un Qmodel.hamiltonian ? alla fine non ci faccio granché. Forse conviene tenerla come variabile passeggera dentro evolve
-    e fine. A quel punto sarebbe:
-    hamiltonian = hamiltonian_eig(L,field, g)
-    e tutto il resto hamiltonian.eigvect, hamiltonian.eigval (non Qmodel.hamiltonian.eigvect, Qmodel.hamiltonian.eigval)
-
-    -L e g così vanno passati sempre ma tenerli dentro Qmodel mi sembrava poco coerente (sono oggetti che riguardano l'hamiltoniana quindi boh.)
     
-    -come faccio ad inizializzare una classe dentro una funzione di un'altra classe? (mi serve per hamiltonian)
-    '''
+if __name__ == "__main__":
+
+    import numpy as np
+    from gif import create_gif
+
+    qstart = np.array([-1/2 - (np.sqrt(5))/2 ,1], dtype=complex)
+    qtarget = np.array([+1/2 + (np.sqrt(5))/2 ,1], dtype=complex)
+    qstart=qstart/np.sqrt(np.vdot(qstart,qstart))
+    qtarget=qtarget/np.sqrt(np.vdot(qtarget,qtarget))
+    t_max=3
+    n_steps=100
+    dt = t_max/n_steps
+
+    model = quantum_model(qstart, qtarget, dt, L=1, g=1, h_list=[-4, 0, 4], history=False)
+    protocol=[0]*100
+    states=model.evolve_from_protocol(protocol)
+    fname = 'test.gif'
+    create_gif(states, qstart, qtarget, fname)
 
 
-#if __name__ == "__main__":
-#
-#    import numpy as np
-#
-#    qtarget = np.array([-1/np.sqrt(4) - 1/np.sqrt(4)*1.j, 1/np.sqrt(2) + 0.j])
-#    qstart = np.array([+1/np.sqrt(4) + 1/np.sqrt(4)*1.j, 1/np.sqrt(2) + 0.j])
-#
-#    model = quantum_model(qstart, qtarget, dt=0.01, history=True)
-#    model.evolve(L=1, field=0, g=1)
-#
-#    print(model.qcurrent)
-#
-#    print(model.compute_fidelity())
 # %%
