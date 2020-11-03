e88a5e9f758f66293ac99a24490f17f5fc69f93a
ceminente <43959050+ceminente@users.noreply.github.com>
2020-11-03 14:16:49 +0100

    Add files via upload

--- SD/SD.py
+++ SD/SD.py
@@ -1,4 +1,5 @@
-from quantum_state import evolution_from_protocol, spectral_time_evolution, compute_fidelity
+from quantum_state import compute_fidelity
+from Qmodel import quantum_model
 import numpy as np
 from random import choices
 from random import uniform
@@ -11,15 +12,18 @@ def exp_dec(iteration,percentage_flip,niterations):
     return percentage_flip * np.exp(-iteration/tau)
 
 def stochastic_descent(qstart, qtarget, L, T, nsteps, iterations, flips,exp_decay_flip, field_list,beta,metropolis_choice,verbose, check_norm):
+    
+    dt = T/nsteps
+    
+    #initialize model
+    model=quantum_model(qstart, qtarget, dt, L, g=1, h_list=field_list, history=True)
 
     np.random.seed(213)
-
-    dt = T/nsteps
 
     random_protocol = np.array(choices(field_list, k=nsteps)) # Define a random protocol, sampling without replacement from a list. 
     temp_protocol=deepcopy(random_protocol)
 
-    fidelity = compute_fidelity(qstart, qtarget)
+    fidelity = model.compute_fidelity()
     fidelity_values=[fidelity]
     '''flips_iter = flips
     break_variable = 0'''
@@ -33,7 +37,7 @@ def stochastic_descent(qstart, qtarget, L, T, nsteps, iterations, flips,exp_deca
 
         for flip in moves: 
 
-            psi = deepcopy(qstart)
+            model.reset()
 
             '''if exp_decay_flip is True:
                 flips_iter=int(exp_dec(iteration=j,percentage_flip=flips,niterations=iterations))'''
@@ -42,15 +46,14 @@ def stochastic_descent(qstart, qtarget, L, T, nsteps, iterations, flips,exp_deca
             temp_protocol=deepcopy(random_protocol)
             temp_protocol[index_update] = random_protocol[index_update]*(-1) # Try to update that index.
             
-            evolution = evolution_from_protocol(psi, qtarget, temp_protocol, spectral_time_evolution, dt, L, make_gif=None)
-            psi = evolution[-1]
+            evolution=model.evolve_from_protocol(temp_protocol)
             
-            if check_norm and (np.abs(1 - compute_fidelity(psi,psi)) > 1e-9):
+            if check_norm and (np.abs(1 - compute_fidelity(evolution[-1],evolution[-1])) > 1e-9):
                 print("Warning ---> Norm is not conserved")
-                print(compute_fidelity(psi,psi))
+                print(compute_fidelity(evolution[-1],evolution[-1]))
                 break
 
-            temp_fidelity = compute_fidelity(qtarget, psi) # Evaluate the fidelity
+            temp_fidelity = model.compute_fidelity() # Evaluate the fidelity
             #break_variable +=1
             if temp_fidelity>fidelity: # Update the change only if better fidelity
                 random_protocol=deepcopy(temp_protocol)
